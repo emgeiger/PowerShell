1907bc59d2625f17a8fcc9717ee4194896a053c6
emgeiger <36167405+emgeiger@users.noreply.github.com>
2022-07-14 10:53:25 -0500

    reverting back, after accidental file deletion in an attampt to move a file, between repos.

--- Reset.ps1
+++ Reset.ps1
@@ -1,3 +1,9 @@
 [System.IO.File]::ReadLines("") | foreach { # % {
-    
+	ping -a -n 2 -w 2000 $_ | Out-Null
+    Test-Connection -Count 2 -TimeToLive 2 $_ | Out-Null
+
+    if($?)
+    {
+		Start-Process "P:\psexec.exe" -ArgumentList "\\$_ -d -e -h -s cmd /c reg import C:\tools\dump.reg"	
+	}
 }
